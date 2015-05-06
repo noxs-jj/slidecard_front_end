dfb33297d4945e2e9a130bafcf0ca68defead9ee
Jean-Jacques MOIROUX <jmoiroux@student.42.fr>
2015-05-06 12:56:27 +0200

    fix register new uer, check email identic, password identic

--- js/controller/register.js
+++ js/controller/register.js
@@ -1,23 +1,36 @@
 console.log('register.js loaded');
 
-slideController.controller('registerController', ['$scope', '$http', '$cookies', '$location', function($scope, $http, $cookies, $location) {
+slideController.controller('registerController',
+		['$scope', '$http', '$cookies', '$location',
+		function($scope, $http, $cookies, $location) {
 	$scope.error = '';
 	$scope.reg = function() {
-		$http.post(urlApi + '/register', {
-			'email': $scope.email,
-			'password': $scope.password,
-			'firstname': $scope.firstname,
-			'lastname': $scope.lastname
-		})
-		.success(function(data) {
-			$cookies.usr_token = data['data'].token;
-			console.log('register success');
-			$location.path('/welcome');
-		})
-		.error(function(data, status) {
-			console.log(error);
-			$scope.error = "Register Failed";
-		})
+		if ($scope.password == $scope.retype_password && $scope.email_again == $scope.email) {
+			$http.post(urlApi + '/register', {
+				'email': $scope.email,
+				'password': $scope.password,
+				'firstname': $scope.firstname,
+				'lastname': $scope.lastname
+			})
+			.success(function(data) {
+				$cookies.usr_token = data['data'].token;
+				console.log('register success');
+				$location.path('/welcome');
+			})
+			.error(function(data, status) {
+				console.log(error);
+				$scope.error = "Register Failed";
+			})
+			console.log('debugg register success');			
+		}
+		else if ($scope.email_again != $scope.email) {
+			$scope.error = "Emails don't match";
+			console.log('debugg register email failed');
+		}
+		else if ($scope.password != $scope.retype_password) {
+			$scope.error = "Passwords don't match";
+			console.log('debugg register password failed');
+		}
 		//REMOVE
 		console.log(
 			$scope.firstname + " / " +
