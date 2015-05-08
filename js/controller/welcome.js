efe0c9282b6fed0e7fe32fb19926c49b58695c78
Thomas BAZIRE <tbazire@e2r7p18.42.fr>
2015-05-08 15:14:32 +0200

    adding menu bar

--- js/controller/welcome.js
+++ js/controller/welcome.js
@@ -3,5 +3,18 @@ console.log('welcome.js loaded');
 slideController.controller('welcomeController',
 	['$scope', '$http', '$cookies', '$location', function($scope, $http, $cookies, $location) {
 
-	$scope.test2 = "test 42 - 1337";
+	if (!$cookies.usr_token)
+		$location.path('/login');
+	else {
+		$scope.error = '';
+		$http.get('http://slidecard.tk/v1/account?token=' + $cookies.usr_token)
+			.success(function(data, status) {
+				$scope.firstname = data.data.firstname;
+				$scope.lastname = data.data.lastname;
+				$cookies.user_id = $scope.id;
+			})
+			.error(function(data, status) {
+				$scope.error = status + ' / ' + data.message;
+			})
+	}
 }]);
