8f90414f3b7f29af81b0ae611765994a3948f432
Thomas BAZIRE <tbazire@e2r7p18.42.fr>
2015-05-14 16:22:33 +0200

    stylish

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 $(function ()
 {
-	$('.header-navbar nav a').click(function ( e )
+	$('.header-navbar ul.dropped').hover(function ()
 	{
+		$('.header-navbar li.dropdown a').addClass('opened');
+	}, function ()
+	{
+		$('.header-navbar li.dropdown a').removeClass('opened');		
+	});
+
+	$('.header-navbar nav > ul > li:not(.dropdown) > a').click(function ( e )
+//	$('.header-navbar a').click(function ( e )
+	{
+//		console.log(e);
 		$('.header-navbar a').removeClass('active');
 		$(this).addClass('active');
+	});
+
+	$('.header-navbar a.brand-title').click(function ( e )
+	{
+		$('.header-navbar a').removeClass('active');
 	});
 });
