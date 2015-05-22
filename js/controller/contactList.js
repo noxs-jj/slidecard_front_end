8e38554b534d03e41db47a9980e5e7de5e06f8a9
Jean-Jacques MOIROUX <jmoiroux@student.42.fr>
2015-05-22 14:55:18 +0200

    contacts en cours

--- js/controller/contactList.js
+++ js/controller/contactList.js
@@ -7,24 +7,26 @@ slideController.controller('contactListController',
 	else {
 		$scope.error = '';
 		$scope.cdn = urlCdn;
-
 		$http.get(urlApi + '/collection?token=' + $cookies.usr_token)
 		.success(function(data, status) {
 			$scope.result = data.data;
+			$scope.removeDeplucate = function arrUnique(arr) {
+				var cleaned = [];
+				$scope.result.forEach(function(itm) {
+					var unique = true;
+					cleaned.forEach(function(itm2) {
+						if (itm.owner.id == itm2.owner.id)
+							unique = false;
+					});
+					if (unique)  cleaned.push(itm);
+				});
+				return cleaned;
+			}
+			$scope.newObj = $scope.removeDeplucate($scope.result);
+
 		})
 		.error(function(data, status) {
 			$scope.error = data.message;
 		})
-
-		// $scope.isPresent = function(id_own) {
-		// 	var i = 0,
-		// 	len = $scope.result.length;
-
-		// 	while (i < len) {
-		// 		if ($scope.result)
-		// 		i++;
-		// 	}
-		// 	return false;
-		// }
 	}
 }]);
